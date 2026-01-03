7b77559c7a3aa18ba2f93bfea17b9c60c6b01e60
Sergio Triana Escobedo <striana@apexsystems.com>
2026-01-03 11:56:58 -0600

    Add Home and Person controllers with views; update .csproj
    Introduce HomeController and PersonController with basic actions and corresponding Razor views. Update project file to add scaffolding package and clean up folder entries.

---
+++ PersonRegisterApp.Web/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonRegisterApp.Web.Controllers;
+
+public class PersonController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+}
