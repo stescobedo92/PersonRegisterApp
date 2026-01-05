3d361cdb2608365ba21e308328e4e45dfe17aa7c
Sergio Triana Escobedo <striana@apexsystems.com>
2026-01-05 08:00:42 -0600

    Add Email and PhoneNumber columns to People table
    Added migration to introduce required Email and PhoneNumber fields to the People table. Updated AppDbContextModelSnapshot to reflect these schema changes, enforcing constraints and data types for both new columns.

--- PersonRegisterApp.Web/Migrations/20260104224628_AddingEmailAndPhoneNumberColumns.Designer.cs
+++ PersonRegisterApp.Web/Migrations/20260104224628_AddingEmailAndPhoneNumberColumns.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonRegisterApp.Web.Models;
 
@@ -10,9 +11,11 @@ using PersonRegisterApp.Web.Models;
 namespace PersonRegisterApp.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260104224628_AddingEmailAndPhoneNumberColumns")]
+    partial class AddingEmailAndPhoneNumberColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PersonRegisterApp.Web/Migrations/20260104224628_AddingEmailAndPhoneNumberColumns.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonRegisterApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingEmailAndPhoneNumberColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "People",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "People");
+        }
+    }
+}
